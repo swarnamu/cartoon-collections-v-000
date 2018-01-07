78cbad759ff9adaec89bc61f6ddcb8fc08da0434
Swarna Muralidharan <swarna.muralic@gmail.com>
2018-01-07 17:26:20 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(names)# code an argument here
   # Your code here
-  name.each_with_index{|name, index| puts "#{index + 1 } #{name}";}
+  names.each_with_index{|name, index| puts "#{index + 1 } #{name}";}
 end
 
 def summon_captain_planet# code an argument here
