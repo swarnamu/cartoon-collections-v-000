bdb8e633d75b48cb60db268e70fe675c9978aaf2
Swarna Muralidharan <swarna.muralic@gmail.com>
2018-01-07 17:32:47 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
-  planeteer_calls.collect{|x| x + "!"}
+  planeteer_calls.collect{|x| x.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
