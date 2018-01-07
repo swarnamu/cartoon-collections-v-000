06a610f4242823898202649853aac30008dd7dc9
Swarna Muralidharan <swarna.muralic@gmail.com>
2018-01-07 17:25:19 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names)# code an argument here
   # Your code here
+  name.each_with_index{|name, index| puts "#{index + 1 } #{name}";}
 end
 
 def summon_captain_planet# code an argument here
